91112effaa2d86d6df4e35d2daefa56143cd4c56
Reaz Tahmidur Rahman <51685766+reaztahmidur@users.noreply.github.com>
2023-09-16 10:16:19 -0400

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,58 +1,31 @@
-#Credit for the logic and idea: https://www.youtube.com/watch?v=FWSR_7kZuYg
 import pygame
 import numpy as np
 import random
 
 class Grid:
     def __init__(self, width, height, scale, offset):
-        #A scaler factor that is deciding size of grids per columns and row s// defined in main.py
-        self.scale = scale  
-        self.columns = int(height/scale)
-        self.rows = int(width/scale)
+        self.scale = scale
+        self.columns = height // scale
+        self.rows = width // scale
         self.size = (self.rows, self.columns)
-        self.grid_array = np.ndarray(shape=(self.size))
-        #Off set to visually distinguish each block 
+        self.grid_array = np.random.randint(2, size=self.size)
         self.offset = offset
 
-    #Iterate through all the possible positions and give a dead or alive value 
-    def random2d_array(self):
-        for x in range(self.rows):
-            for y in range(self.columns):
-                self.grid_array[x][y] = random.randint(0,1)
-
-    #Use Conway's properties             
     def Conway(self, off_color, on_color, surface, pause):
         for x in range(self.rows):
             for y in range(self.columns):
-                y_pos = y * self.scale
-                x_pos = x * self.scale
-                if self.grid_array[x][y] == 1:
-                    pygame.draw.rect(surface, on_color, [x_pos, y_pos, self.scale-self.offset, self.scale-self.offset])
-                else:
-                    pygame.draw.rect(surface, off_color, [x_pos, y_pos, self.scale-self.offset, self.scale-self.offset])
+                y_pos, x_pos = y * self.scale, x * self.scale
+                color = on_color if self.grid_array[x][y] == 1 else off_color
+                pygame.draw.rect(surface, color, [x_pos, y_pos, self.scale - self.offset, self.scale - self.offset])
 
-        next = np.ndarray(shape=(self.size))
-        if pause == False:
+        if not pause:
+            next = np.copy(self.grid_array)
             for x in range(self.rows):
                 for y in range(self.columns):
-                    state = self.grid_array[x][y]
-                    neighbors = self.get_neighbors( x, y)
-                    if state == 0 and neighbors == 3:
-                        next[x][y] = 1
-                    elif state == 1 and (neighbors < 2 or neighbors > 3):
-                        next[x][y] = 0
-                    else:
-                        next[x][y] = state
+                    state, neighbors = self.grid_array[x][y], self.get_neighbors(x, y)
+                    next[x][y] = 1 if (state == 0 and neighbors == 3) or (state == 1 and 2 <= neighbors <= 3) else 0
             self.grid_array = next
-            
-    #Check to see surrounding alive/ dead memebers to update accordingly        
-    def get_neighbors(self, x, y):
-        total = 0
-        for n in range(-1, 2):
-            for m in range(-1, 2):
-                x_edge = (x+n+self.rows) % self.rows
-                y_edge = (y+m+self.columns) % self.columns
-                total += self.grid_array[x_edge][y_edge]
 
-        total -= self.grid_array[x][y]
-        return total
+    def get_neighbors(self, x, y):
+        total = sum(self.grid_array[(x + n) % self.rows][(y + m) % self.columns] for n in range(-1, 2) for m in range(-1, 2))
+        return total - self.grid_array[x][y]
